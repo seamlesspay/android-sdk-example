aa9395dc9c17192b3b1653bba6a25ca5b83bf62e
Michael <undermyclouds@gmail.com>
2025-04-29 12:41:13 +0300

    removed dynamic title for theme switcher

--- example/src/main/java/com/seamlesspay/demo/screens/HomeFragment.kt
+++ example/src/main/java/com/seamlesspay/demo/screens/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.seamlesspay.demo.base.BaseFragment
-import com.seamlesspay.demo.util.buildDebugInfo
 import com.seamlesspay.demo.util.forceDarkMode
 import com.seamlesspay.demo.util.isDarkMode
 import com.seamlesspay.example.R
@@ -17,12 +16,9 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
   override fun FragmentHomeBinding.initViews() {
     // Set up Theme Switching
 
-    val isDarkMode = requireContext().isDarkMode()
-    switchDarkMode.isChecked = isDarkMode
-    updateSwitchTitle(isDarkMode)
+    switchDarkMode.isChecked = requireContext().isDarkMode()
     switchDarkMode.setOnCheckedChangeListener { _, b ->
       forceDarkMode(b)
-      updateSwitchTitle(b)
     }
 
     // Set up Controls
@@ -36,13 +32,5 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     }
 
     //tvAppData.text = buildDebugInfo(requireContext(), "2.2.5-rc1")
-  }
-
-  private fun updateSwitchTitle(darkMode: Boolean) {
-    binding.textDarkModeLabel.text = if (darkMode) {
-      getString(R.string.light_mode_title)
-    } else {
-      getString(R.string.dark_mode_title)
-    }
   }
 }
